993c60d3bdfea9335f1d3e3bf8d4a62db3546b12
Arthals <zhuozhiyongde@126.com>
2023-02-07 22:37:08 +0800

    optimize & bug fix

--- BracketMatchAndPanguSpace.py
+++ BracketMatchAndPanguSpace.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 #@Author  :   Arthals
-#@File    :   BracketMatchAndPanguSpace.py
-#@Time    :   2023/02/02 20:35:56
+#@File    :   try.py
+#@Time    :   2023/02/07 19:36:35
 #@Contact :   zhuozhiyongde@126.com
 #@Software:   Visual Studio Code
 
@@ -11,204 +11,235 @@ import pangu
 import argparse
 
 
-def match_brackets(origin_text: str):
-    # 忽略图片行、空行
-    pattern = re.compile(r'(<img src)|!\[.*?\]\(.*?\)|^\n$')
-    if pattern.search(origin_text):
-        return origin_text
-    # 将所有中文圆括号替换为英文圆括号
-    text = origin_text.replace('（', '(').replace('）', ')')
+def process_text(origin_text: str, do_match_brackets: bool,
+                 do_pangu_format: bool) -> list:
+    '''
+    处理文本，返回处理后结果列表
 
-    # 使用栈检查括号匹配
-    stack = []
-    for i in range(len(text)):
-        if text[i] == '(':
-            stack.append(text[i])
-        elif text[i] == ')':
-            if not stack or stack[-1] != '(':
-                return False
-            stack.pop()
+    Parameters
+    ----------
+    origin_text : str
+        原始文本
+    do_match_brackets : bool
+        是否进行括号匹配
+    do_pangu_format : bool
+        是否进行格式化
+    
+    Returns
+    -------
+    list
+        处理后结果列表
+        list[0] : int, -1:括号匹配失败, 0:未操作, 1:括号匹配成功
+        list[1] : int, -1:格式化失败, 0:未操作, 1:格式化成功
+        list[2] : str, 处理后文本
+    '''
 
-    if stack:
-        return False
+    def match_brackets(origin_text: str) -> str:
+        # 将所有中文圆括号替换为英文圆括号
+        text = origin_text.replace('（', '(').replace('）', ')')
 
-    # 已知忽略中英文差别下括号匹配，递归从左到右按照左括号类型进行匹配
-    new_text = origin_text
-    left_brackets_stack = []
-    for i in range(len(origin_text)):
-        if re.match(r'\(|（', origin_text[i]):
-            left_brackets_stack.append(origin_text[i])
-        elif re.match(r'\)|）', origin_text[i]):
-            right_bracket = ')' if left_brackets_stack[-1] == '(' else '）'
-            left_brackets_stack.pop()
-            new_text = new_text[:i] + right_bracket + new_text[i + 1:]
+        # 使用栈检查括号匹配
+        stack = []
+        for i in range(len(text)):
+            if text[i] == '(':
+                stack.append(text[i])
+            elif text[i] == ')':
+                if not stack or stack[-1] != '(':
+                    return False
+                stack.pop()
 
-    # 以下为自定义的其他替换规则
-    # 目的：只要左括号左右字符任一为中文，就将左右括号替换为中文括号
-    # 将所有形如`中文()`的类型递归替换为`中文（）`
-    pattern = re.compile(r'(?<=[\u4e00-\u9fff])\(([^(]*?)\)')
-    while True:
-        text = new_text
-        new_text = re.sub(pattern, '（\\1）', new_text)
-        if text == new_text:
-            break
+        if stack:
+            return False
 
-    # 将所有形如`(第一个字符为中文)`的类型递归替换为`（文本）`
-    pattern = re.compile(r'\(([\u4e00-\u9fff][^\(]*?)\)')
-    while True:
-        text = new_text
-        new_text = re.sub(pattern, '（\\1）', new_text)
-        if text == new_text:
-            break
+        # 已知忽略中英文差别下括号匹配，递归从左到右按照左括号类型进行匹配
+        new_text = origin_text
+        left_brackets_stack = []
+        for i in range(len(origin_text)):
+            if re.match(r'\(|（', origin_text[i]):
+                left_brackets_stack.append(origin_text[i])
+            elif re.match(r'\)|）', origin_text[i]):
+                right_bracket = ')' if left_brackets_stack[-1] == '(' else '）'
+                left_brackets_stack.pop()
+                new_text = new_text[:i] + right_bracket + new_text[i + 1:]
 
-    return new_text
+        # 以下为自定义的其他替换规则
+        # 目的：只要左括号左右字符任一为中文，就将左右括号替换为中文括号
+        # 将所有形如`中文()`的类型递归替换为`中文（）`
+        pattern = re.compile(r'(?<=[\u4e00-\u9fff])\(([^(]*?)\)')
+        while True:
+            text = new_text
+            new_text = re.sub(pattern, '（\\1）', new_text)
+            if text == new_text:
+                break
 
+        # 将所有形如`(第一个字符为中文)`的类型递归替换为`（文本）`
+        pattern = re.compile(r'\(([\u4e00-\u9fff][^\(]*?)\)')
+        while True:
+            text = new_text
+            new_text = re.sub(pattern, '（\\1）', new_text)
+            if text == new_text:
+                break
 
-def find_inline_latex(text: str):
-    latex_blocks = re.compile(
-        r'(?s)(?<!\\)\$\$(.*?)(?<!\\)\$\$|(?<!\\)\$(.*?)(?<!\\)\$')
-    return [(m.start(), m.end()) for m in latex_blocks.finditer(text)]
+        return new_text
 
+    def find_inline_latex(text: str):
+        latex_blocks = re.compile(
+            r'(?s)(?<!\\)\$\$(.*?)(?<!\\)\$\$|(?<!\\)\$(.*?)(?<!\\)\$')
+        return [(m.start(), m.end()) for m in latex_blocks.finditer(text)]
 
-def pangu_format_line(line: str) -> str:
-    # 忽略图片行、空行
-    pattern = re.compile(r'(<img src)|!\[.*?\]\(.*?\)|^\n$')
-    if pattern.search(line):
-        return line
+    text = origin_text
+    return_result = [0, 0, origin_text]
 
-    latex_blocks = find_inline_latex(line)
-    result = []
+    # 存储所有的latex块
+    latex_blocks = find_inline_latex(text)
+    latex_map = {}
+    seprated_text = []
     start = 0
-    for start_inx, end_inx in latex_blocks:
-        result.append(pangu.spacing_text(line[start:start_inx]))
-        result.append(line[start_inx:end_inx])
-        start = end_inx
-    result.append(pangu.spacing_text(line[start:]))
-    new_line = ''.join(result)
+    for i, (start_pos, end_pos) in enumerate(latex_blocks):
+        if start != start_pos:
+            seprated_text.append(text[start:start_pos])
+        if start_pos == 0:
+            seprated_text.append('')
 
-    # 去除 Pangu.js 错误的在加粗语法**text**中添加的空格
-    pattern = re.compile(r'(?<=\*\*)[^\*]*?(?=\*\*)')
-    new_line = pattern.sub(lambda x: x.group().strip(), new_line)
+        latex_map[i] = text[start_pos:end_pos]
+        start = end_pos
 
-    # Pangu.js 最后调用了 .strip() 方法，还会移除行首空格，这里补上
-    if re.match(r' +', line):
-        new_line = re.match(r' +', line).group() + re.sub(r'^ +', '', new_line)
+    if start != len(text):
+        seprated_text.append(text[start:])
 
-    # 同上原因，导致最后一行的换行符被去除，这里补上
-    if not new_line.endswith('\n'):
-        new_line += '\n'
+    # 处理括号匹配
+    if do_match_brackets:
+        # save seperated text lens
+        seprated_text_lens = [len(s) for s in seprated_text]
 
-    return new_line
+        text = ''.join(seprated_text)
+        text = match_brackets(text)
+
+        if text is False:
+            return_result[0] = -1
+
+        # restore seperated text
+        else:
+            for i in range(len(seprated_text_lens)):
+                if seprated_text[i] != text[:seprated_text_lens[i]]:
+                    seprated_text[i] = text[:seprated_text_lens[i]]
+                    return_result[0] = 1
+
+                text = text[seprated_text_lens[i]:]
+
+    # 处理 pangu.js
+    if do_pangu_format:
+        result = []
+        for index in range(len(seprated_text)):
+            result.append(pangu.spacing_text(seprated_text[index]))
+            if index in latex_map:
+                result.append(latex_map[index])
+
+        text = ''.join(result)
+
+        # 去除 Pangu.js 错误的在加粗语法**text**中添加的空格
+        pattern = re.compile(r'(?<=\*\*)[^\*]*?(?=\*\*)')
+        text = pattern.sub(lambda x: x.group().strip(), text)
+        pattern = re.compile(r' *(\*\*[^\*]*?\*\*) *')
+        text = pattern.sub(' \\1 ', text)
+        text = re.sub(r'^ \*\*', '**', text)
+        text = re.sub(r'\*\* $', '**', text)
+
+        # 同上原因，导致最后一行的换行符被去除，这里补上
+        if not text.endswith('\n'):
+            text += '\n'
+
+        if text != origin_text:
+            return_result[1] = 1
+            return_result[2] = text
+
+    return return_result
 
 
 def format_file(file: str,
                 show_info: bool = True,
-                do_bracket_format: bool = True,
+                do_match_brackets: bool = True,
                 do_pangu_format: bool = True):
     if not file.endswith(('.md', 'markdown')):
         return False
 
-    if not do_bracket_format and not do_pangu_format:
+    if not do_match_brackets and not do_pangu_format:
         return False
 
     with open(file, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
-    # 匹配括号
-    bracket_flag = False
     code_block_flag = False
     latex_block_flag = False
-    code_block_index = []
-    latex_block_index = []
-    match_flag = False
+
+    # 匹配括号
+    do_bracket_flag = False
+    do_pangu_flag = False
+    error_match_flag = False
     for i in range(len(lines)):
-        line = lines[i]
-
         # 忽略代码块和行内公式
-        if re.match(r'```', line):
-            if code_block_flag is True:
-                code_block_index[-1].append(i)
-            else:
-                code_block_index.append([i])
+        if code_block_flag or latex_block_flag:
+            continue
 
+        line = lines[i]
+        # 忽略图片行、空行
+        pattern = re.compile(r'(<img src)|!\[.*?\]\(.*?\)|^\n$')
+        if pattern.search(line):
+            continue
+
+        if re.match(r'```', line):
             code_block_flag = not code_block_flag
             continue
 
         elif re.match(r'\$\$', line):
             if not code_block_flag:
-                if latex_block_flag is True:
-                    latex_block_index[-1].append(i)
-                else:
-                    latex_block_index.append([i])
-
+                if re.match(r'\$\$.*\$\$', line):
+                    print(line)
+                    continue
                 latex_block_flag = not latex_block_flag
                 continue
 
-        if not do_bracket_format:
-            continue
+        result = process_text(line, do_match_brackets, do_pangu_format)
 
-        if re.search(r'[\(\)（）]', line):
-            new_line = match_brackets(line)
-            if new_line is False:
-                print('Error in file: %s, line: %d' % (file, i + 1))
-                print('Error line: %s' % line)
-                match_flag = True
-                continue
+        if result[0] == -1:
+            print('File: %s Match brackets Error, line: %d' % (file, i + 1))
+            print('File: %s Error line: %s' % (file, line))
+            error_match_flag = True
 
-            if new_line == line:
-                continue
+        if result[0] == 1:
+            print('File: %s, line: %d brackets matched' % (file, i + 1))
+            do_bracket_flag = True
 
-            lines[i] = new_line
-            bracket_flag = True
-            print('File: %s, line: %d bracket formatted' % (file, i + 1))
-
-    if not do_bracket_format:
-        if not bracket_flag and not match_flag:
-            print('File: %s, no bracket error' % file)
-
-        if match_flag:
-            print('File: %s, bracket match error' % file)
-
-    # pangu format lines
-    pangu_flag = False
-    new_lines = []
-    ignore_lines = [item for sublist in code_block_index for item in sublist] + \
-        [item for sublist in latex_block_index for item in sublist]
-    # 去重（其实不必要）
-    ignore_lines = list(set(ignore_lines))
-
-    for i in range(len(lines)):
-        # 忽略代码块、公式块
-        if i in ignore_lines:
-            new_lines.append(lines[i])
-            continue
-
-        if not do_pangu_format:
-            new_lines.append(lines[i])
-            continue
-
-        new_line = pangu_format_line(lines[i])
-
-        new_lines.append(new_line)
-        if new_line != lines[i]:
+        if result[1] == 1:
             print('File: %s, line: %d pangu formatted' % (file, i + 1))
+            do_pangu_flag = True
+
             if show_info:
-                print([lines[i]], [new_line], '', sep='\n')
-            pangu_flag = True
+                print([lines[i]], [result[2]], sep='\n')
 
-    if not do_pangu_format:
-        if not pangu_flag:
-            print('File: %s, no pangu format' % file)
+        lines[i] = result[2]
 
-    # pangu format lines and write to file
+    # print result
+    print('Summary for File: %s' % file)
+    if error_match_flag:
+        print('File: %s Match brackets Error' % file)
+    if do_bracket_flag:
+        print('File: %s Match brackets Success' % file)
+    else:
+        print('File: %s Match brackets No need' % file)
+    if do_pangu_flag:
+        print('File: %s Pangu format Success' % file)
+    else:
+        print('File: %s Pangu format No need' % file)
+    print('----------------------------------------')
+
     with open(file, 'w', encoding='utf-8') as f:
-        f.writelines(new_lines)
+        f.writelines(lines)
 
 
 parser = argparse.ArgumentParser(
     description=
     'Format markdown file, including pangu format and bracket match.')
-
 parser.add_argument('path',
                     type=str,
                     nargs='?',
@@ -237,7 +268,7 @@ if __name__ == '__main__':
 
     if path.endswith(('.md', '.markdown')):
         format_file(path,
-                    do_bracket_format=do_bracket,
+                    do_match_brackets=do_bracket,
                     do_pangu_format=do_pangu)
 
     elif os.path.isdir(path):
@@ -251,14 +282,15 @@ if __name__ == '__main__':
             for file in md_files:
                 format_file(file,
                             show_info=False,
-                            do_bracket_format=do_bracket,
+                            do_match_brackets=do_bracket,
                             do_pangu_format=do_pangu)
         else:
             md_files = [
-                file for file in os.listdir(path) if file.endswith(('.md', '.markdown'))
+                file for file in os.listdir(path)
+                if file.endswith(('.md', '.markdown'))
             ]
             for file in md_files:
                 format_file(os.path.join(path, file),
                             show_info=False,
-                            do_bracket_format=do_bracket,
+                            do_match_brackets=do_bracket,
                             do_pangu_format=do_pangu)
